d9ab1722135074e9346ec04e4a7a75c00e9d7e86
Yuvraj Dube <yuvrajdube1623@gmail.com>
2025-06-16 23:36:29 +0530

    Auth screens

--- app/src/main/java/com/example/maatritva/MainActivity.kt
+++ app/src/main/java/com/example/maatritva/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.maatritva
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.maatritva.ui.pregscreen.PregnancyTrackerApp
+import com.example.maatritva.ui.Auth.AuthViewModel
+import com.example.maatritva.ui.Auth.MyAppNavigation
 import com.example.maatritva.ui.theme.MaatritvaTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val authViewModel : AuthViewModel by viewModels()
         setContent {
             MaatritvaTheme {
-
-                PregnancyTrackerApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyAppNavigation(modifier =  Modifier.padding(innerPadding), authViewModel = authViewModel)
+                }
+//                PregnancyTrackerApp()
                 }
             }
         }

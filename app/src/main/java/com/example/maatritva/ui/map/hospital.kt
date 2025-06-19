4cb032070a51b0edd077a8ff129ff387e44ed68d
Studggle <vg1992005@gmail.com>
2025-06-20 02:11:59 +0530

    Update hospital.kt

--- app/src/main/java/com/example/maatritva/ui/map/hospital.kt
+++ app/src/main/java/com/example/maatritva/ui/map/hospital.kt
@@ -6,19 +6,37 @@ import android.location.Location
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChildFriendly
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.maatritva.ui.homescreen.AppHeader
+import com.example.maatritva.ui.theme.DarkPink
+import com.example.maatritva.ui.theme.LightPink
+import com.example.maatritva.ui.theme.MediumPink
+import com.example.maatritva.ui.theme.Pink40
+import com.example.maatritva.ui.theme.Pink80
 import com.example.maatritva.ui.theme.Red40
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
@@ -62,9 +80,9 @@ fun Hospital() {
 
     Column(
         modifier = Modifier.fillMaxSize().background(Color(0xFFF5F5F5)),
-        horizontalAlignment = Alignment.CenterHorizontally
+//        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AppHeader()
+        AppHeader1()
 
         Box(modifier = Modifier
             .height(350.dp).padding(16.dp), contentAlignment = Alignment.Center) {
@@ -88,25 +106,117 @@ fun Hospital() {
                             state = MarkerState(
                                 position = LatLng(userLocation!!.latitude + 0.002, userLocation!!.longitude + 0.002)
                             ),
-                            title = "Nearby Hospital",
-                            snippet = "Example Hospital"
+                            title = "Om Maternity Center",
+                            snippet = "Example Hospital-1"
+                        )
+                        Marker(
+                            state = MarkerState(
+                                position = LatLng(userLocation!!.latitude + 0.000, userLocation!!.longitude + 0.006)
+                            ),
+                            title = "Uttam Hospital",
+                            snippet = "Example Hospital-2"
                         )
                     }
                 }
             }
         }
+        Text("Nearby",modifier = Modifier.padding(horizontal = 16.dp,10.dp), fontSize = 20.sp, fontWeight = FontWeight.Bold, color = DarkPink)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(
+                        Color(0xFFD33560),
+                        RoundedCornerShape(12.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Uttam Hospital",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Text(
+                    text = "Jankipuam Extension, Lucknow",
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(
+                        Color(0xFFD33560),
+                        RoundedCornerShape(12.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = "Om Maternity Center",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = "South City (Raebarely Road), Lucknow",
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
+                }
+        }
+
     }
 
 }
 
 
 @Composable
-fun AppHeader() {
+fun AppHeader1() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .background(Red40)
-            .padding(top = 30.dp)
+            .padding(top = 50.dp)
     ) {
         Text(
             modifier = Modifier.padding(16.dp),
